d1ae9b9c65a8b4d52414da7653eecf34201a0dd1
Abu Talha <talha@webozza.com>
2024-05-23 17:01:06 +0600

    zip filter added

--- functions.php
+++ functions.php
@@ -2,9 +2,29 @@
 /**
  * MyBag Child
  *
- * @package mybag-child
+ * @package mybag-child 
  */
 
 /**
  * Include all your custom code here
  */
+
+
+
+// Enqueue the CSS and JS files
+function enqueue_zip_code_checker_assets() {
+    if (is_product()) {
+        wp_enqueue_style('zip-code-checker-css', get_stylesheet_directory_uri() . '/css/zip_code_checker.css');
+        wp_enqueue_script('zip-code-checker-js', get_stylesheet_directory_uri() . '/js/zip_code_checker.js', array(), null, true);
+    }
+}
+add_action('wp_enqueue_scripts', 'enqueue_zip_code_checker_assets');
+
+// Include the ZIP code checker template
+function include_zip_code_checker() {
+    if (is_product()) {
+        include get_stylesheet_directory() . '/woocommerce/zip_code_checker.php';
+        display_zip_code_checker();
+    }
+}
+add_action('woocommerce_before_single_product_summary', 'include_zip_code_checker', 20);
---
+++ js/zip_code_checker.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('check_zip_code').addEventListener('click', function() {
+//         var zipCode = document.getElementById('zip_code').value;
+//         var resultDiv = document.getElementById('zip_code_result');
+        
+//         if (available_zip_code.incudes(zipCode)) {
+//             resultDiv.innerHTML = '<span class="zip-available">YES, get started <a href="#">here</a></span>';
+//         } else {
+//             resultDiv.innerHTML = '<span class="zip-not-available">Unfortunately not at this time</span>';
+//         }
+//     });
+// });
