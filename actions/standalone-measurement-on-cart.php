3a81d970a101f1fb4c593bcb4c554ad23658bfac
webozza <webozza@gmail.com>
2024-11-21 10:48:32 +0600

    version update

--- actions/standalone-measurement-on-cart.php
+++ actions/standalone-measurement-on-cart.php
@@ -1,18 +1,26 @@
 <?php
 
-function remove_all_products_if_specific_product_added( $cart_item_key, $product_id ) {
-    $target_product_id = 1056; // The product ID to check
+function handle_cart_logic_for_target_product( $cart_item_key, $product_id ) {
+    $target_product_id = 1056; // The product ID to check and manage
+
+    // Get the current cart contents
+    $cart = WC()->cart->get_cart();
 
     if ( $product_id == $target_product_id ) {
-        // Get the current cart contents
-        $cart = WC()->cart->get_cart();
-
-        // Loop through the cart and remove all other items
+        // If the target product is added, remove all other products
         foreach ( $cart as $key => $item ) {
-            if ( $key !== $cart_item_key ) {
+            if ( $item['product_id'] != $target_product_id ) {
+                WC()->cart->remove_cart_item( $key );
+            }
+        }
+    } else {
+        // If any other product is added, remove the target product
+        foreach ( $cart as $key => $item ) {
+            if ( $item['product_id'] == $target_product_id ) {
                 WC()->cart->remove_cart_item( $key );
             }
         }
     }
 }
-add_action( 'woocommerce_add_to_cart', 'remove_all_products_if_specific_product_added', 10, 2 );
+
+add_action( 'woocommerce_add_to_cart', 'handle_cart_logic_for_target_product', 10, 2 );
