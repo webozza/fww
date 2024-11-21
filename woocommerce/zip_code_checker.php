b7844775b30047fd36c6ae6d56970d67e2c1fd7f
webozza <webozza@gmail.com>
2024-11-21 09:13:41 +0600

    version update

--- woocommerce/zip_code_checker.php
+++ woocommerce/zip_code_checker.php
@@ -19,7 +19,8 @@ function display_zip_code_checker() {
     <script>
         // Pass the PHP variables to JavaScript
         let available_zip_codes = "<?php echo esc_js($available_zip_codes); ?>".split(',');
-         let sucess_text =  `<span class="zip-available"><img src="<?= $tick_icon?>"> YES, get started <a href="/measuring">click here</a>`;
+         // let sucess_text =  `<span class="zip-available"><img src="<?= $tick_icon?>"> YES, get started <a href="/measuring">click here</a>`;
+         let sucess_text =  `<span class="zip-available"><img src="<?= $tick_icon?>"> YES, get started <a href="/product/2-cordless-faux-wood-blinds-new?measuring=available">Measurement Service<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"/></svg></a>`;
          let unavailable_text = `<span class="zip-not-available"><img src="<?= $cros_icon?>"> Unfortunately not at this time</span>`
          
          console.log(sucess_text);
