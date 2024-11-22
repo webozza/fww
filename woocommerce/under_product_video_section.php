1f823aa17ac1cce12dc90a654038973a39c0369b
webozza <webozza@gmail.com>
2024-11-22 13:30:08 +0600

    version update

--- woocommerce/under_product_video_section.php
+++ woocommerce/under_product_video_section.php
@@ -7,21 +7,21 @@
 <div class="video_section">
     <div class="video_container">
         <h2>Inside or Outside Mount</h2>
-        <video poster="/wp-content/uploads/2024/10/video_placeholder.png" playsinline preload="auto" style="width: 100%; height: 100%;" class="vjs-tech" id="player_inside_outside_mount">
+        <video poster="/wp-content/uploads/2024/10/video_placeholder.png">
             <source type="video/mp4" src="/wp-content/uploads/2024/11/Faux-Wood-Blinds-Deciding-on-Inside-or-Outside-Mount-Blinds.mp4">
             Your browser does not support the video tag.
         </video>
     </div>
     <div class="video_container">
         <h2>Measure Inside Mount</h2>
-        <video poster="/wp-content/uploads/2024/10/video_placeholder.png" playsinline preload="auto" style="width: 100%; height: 100%;" class="vjs-tech" id="player_inside_mount">
+        <video>
             <source type="video/mp4" src="/wp-content/uploads/2024/11/Faux-Wood-Warehouse_-Measuring-for-an-Inside-Mount-Blind.mp4">
             Your browser does not support the video tag.
         </video>
     </div>
     <div class="video_container">
         <h2>Measure Outside Mount</h2>
-        <video poster="/wp-content/uploads/2024/10/video_placeholder.png" playsinline preload="auto" style="width: 100%; height: 100%;" class="vjs-tech" id="player_outside_mount">
+        <video>
             <source type="video/mp4" src="/wp-content/uploads/2024/11/Faux-Wood-Warehouse_-Measuring-for-an-Outside-Mount-Blind.mp4">
             Your browser does not support the video tag.
         </video>
