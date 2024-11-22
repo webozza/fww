d8891719517cdecea90b343c24232ba611b55810
webozza <webozza@gmail.com>
2024-11-22 13:27:06 +0600

    version update

--- woocommerce/under_product_video_section.php
+++ woocommerce/under_product_video_section.php
@@ -7,20 +7,20 @@
 <div class="video_section">
     <div class="video_container">
         <h2>Inside or Outside Mount</h2>
-        <video tabindex="-1" poster="https://fauxwoodwarehouse.com/wp-content/uploads/2024/10/video_placeholder.png" playsinline="playsinline" preload="auto" style="width: 100%; height: 100%;" class="vjs-tech" id="player_html5_api" src="https://fauxwoodwarehouse.com/wp-content/uploads/2024/11/Faux-Wood-Blinds-Deciding-on-Inside-or-Outside-Mount-Blinds.mp4">
-            <source type="video/mp4" src="https://fauxwoodwarehouse.com/wp-content/uploads/2024/11/Faux-Wood-Blinds-Deciding-on-Inside-or-Outside-Mount-Blinds.mp4" label="">
+        <video poster="/wp-content/uploads/2024/10/video_placeholder.png" playsinline="playsinline" preload="auto" style="width: 100%; height: 100%;" class="vjs-tech" id="player_html5_api" src="/wp-content/uploads/2024/11/Faux-Wood-Blinds-Deciding-on-Inside-or-Outside-Mount-Blinds.mp4">
+            <source type="video/mp4" src="/wp-content/uploads/2024/11/Faux-Wood-Blinds-Deciding-on-Inside-or-Outside-Mount-Blinds.mp4" label="">
         </video>
     </div>
     <div class="video_container">
         <h2>Measure Inside Mount</h2>
-        <video tabindex="-1" poster="https://fauxwoodwarehouse.com/wp-content/uploads/2024/10/video_placeholder.png" playsinline="playsinline" preload="auto" style="width: 100%; height: 100%;" class="vjs-tech" id="player_html5_api" src="https://fauxwoodwarehouse.com/wp-content/uploads/2024/11/Faux-Wood-Warehouse_-Measuring-for-an-Inside-Mount-Blind.mp4">
-            <source type="video/mp4" src="https://fauxwoodwarehouse.com/wp-content/uploads/2024/11/Faux-Wood-Warehouse_-Measuring-for-an-Inside-Mount-Blind.mp4" label="">
+        <video poster="/wp-content/uploads/2024/10/video_placeholder.png" playsinline="playsinline" preload="auto" style="width: 100%; height: 100%;" class="vjs-tech" id="player_html5_api" src="/wp-content/uploads/2024/11/Faux-Wood-Warehouse_-Measuring-for-an-Inside-Mount-Blind.mp4">
+            <source type="video/mp4" src="/wp-content/uploads/2024/11/Faux-Wood-Warehouse_-Measuring-for-an-Inside-Mount-Blind.mp4" label="">
         </video>
     </div>
     <div class="video_container">
         <h2>Measure Outside Mount</h2>
-        <video tabindex="-1" poster="https://fauxwoodwarehouse.com/wp-content/uploads/2024/10/video_placeholder.png" playsinline="playsinline" preload="auto" style="width: 100%; height: 100%;" class="vjs-tech" id="player_html5_api" src="https://fauxwoodwarehouse.com/wp-content/uploads/2024/11/Faux-Wood-Warehouse_-Measuring-for-an-Outside-Mount-Blind.mp4">
-            <source type="video/mp4" src="https://fauxwoodwarehouse.com/wp-content/uploads/2024/11/Faux-Wood-Warehouse_-Measuring-for-an-Outside-Mount-Blind.mp4" label="">
+        <video poster="/wp-content/uploads/2024/10/video_placeholder.png" playsinline="playsinline" preload="auto" style="width: 100%; height: 100%;" class="vjs-tech" id="player_html5_api" src="/wp-content/uploads/2024/11/Faux-Wood-Warehouse_-Measuring-for-an-Outside-Mount-Blind.mp4">
+            <source type="video/mp4" src="/wp-content/uploads/2024/11/Faux-Wood-Warehouse_-Measuring-for-an-Outside-Mount-Blind.mp4" label="">
         </video>
     </div>
 </div>
