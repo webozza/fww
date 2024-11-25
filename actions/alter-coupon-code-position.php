8bd17b17d56dccddca41e121a1cee4f4b3202272
webozza <webozza@gmail.com>
2024-11-25 12:04:50 +0600

    version update

--- actions/alter-coupon-code-position.php
+++ actions/alter-coupon-code-position.php
@@ -3,10 +3,10 @@
 add_action('woocommerce_review_order_after_order_total', function () {
     ?>
     <div id="custom-coupon-box">
-        <label for="custom_coupon_code" style="display: block; font-weight: bold; margin-bottom: 10px;">Enter your coupon code:</label>
+        <label for="custom_coupon_code" style="display: block; font-weight: bold; margin-bottom: 10px;">Have a discount code?</label>
         <input placeholder="discount code" type="text" id="custom_coupon_code" style="width: calc(100% - 110px); padding: 10px; border: 1px solid #ddd; border-radius: 4px; margin-right: 10px;" />
-        <button id="apply_custom_coupon" style="padding: 10px 20px; background-color: #af312e; color: #fff; border: none; border-radius: 4px; text-transform: uppercase; cursor: pointer;">Apply Code</button>
-        <div id="coupon-loading" style="display: none; margin-top: 10px; font-size: 14px; color: #52A37F;">Applying coupon...</div>
+        <button id="apply_custom_coupon" style="padding: 10px 20px; background-color: #af312e; color: #fff; border: none; border-radius: 4px; text-transform: uppercase; cursor: pointer;">enter here</button>
+        <div id="coupon-loading" style="display: none; margin-top: 10px; font-size: 14px; color: #52A37F;">Applying discount...</div>
     </div>
     <?php
 });
