f89425f1369dc20582fd925303043428de632465
webozza <webozza@gmail.com>
2024-12-05 14:28:29 +0600

    version update

--- js/global.js
+++ js/global.js
@@ -201,6 +201,13 @@ jQuery(document).ready(function ($) {
     });
   };
 
+  let replaceTwitterIcon = async () => {
+    $(".list-social-icons .fa-twitter").append(`
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+      `);
+    $(".list-social-icons .fa-twitter").removeClass("fa-twitter");
+  };
+
   if (isCart || isCheckout) {
     noShippingForMeasurement();
     alterCartTextFITrite();
@@ -220,4 +227,6 @@ jQuery(document).ready(function ($) {
   if (isSupport) {
     videoPluginModifier();
   }
+
+  replaceTwitterIcon();
 });
